9fdf04c15f94569d20d5326e7ca8c9b4b9e1bd05
Daniel Mathews <dannyl.mathews@gmail.com>
2016-08-03 09:16:31 -0700

    v1

--- W6D3 - Core Animation/Core-Animation-Playground.playground/Contents.swift
+++ W6D3 - Core Animation/Core-Animation-Playground.playground/Contents.swift
@@ -8,43 +8,50 @@ containerView.backgroundColor = UIColor.whiteColor()
 
 XCPlaygroundPage.currentPage.liveView = containerView
 
-let greenView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-greenView.backgroundColor = UIColor.greenColor()
-greenView.center = containerView.center
-greenView.layer.cornerRadius = greenView.frame.width/2
-containerView.addSubview(greenView)
 
+//1. Add a green circle to your container
 
-//Basic Animation. Make a red cicle change from a circle to a square.
+//: ![Alternate text](1-green-circle.png width="width" height="height" poster="poster")
 
 //let greenView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
 //greenView.backgroundColor = UIColor.greenColor()
 //greenView.center = containerView.center
+//greenView.layer.cornerRadius = greenView.frame.width/2
+//containerView.addSubview(greenView)
+
+
+//2. Basic Animation. Make a green cicle to a square.
+
+//: ![Alternate text](2-circle-to-square.gif width="width" height="height" poster="poster")
+
+//let greenView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+//greenView.backgroundColor = UIColor.greenColor()
+//greenView.center = containerView.center
+//greenView.layer.cornerRadius = greenView.frame.size.width/2
 //containerView.addSubview(greenView)
 //
 //let cornerRadiusAnimation = CABasicAnimation(keyPath: "cornerRadius")
-//cornerRadiusAnimation.duration = 1
-//cornerRadiusAnimation.fromValue = 0
-//cornerRadiusAnimation.toValue = greenView.frame.width/2
+//cornerRadiusAnimation.fromValue = greenView.frame.size.width/2
+//cornerRadiusAnimation.toValue = 0
+//cornerRadiusAnimation.duration = 1.5
+//cornerRadiusAnimation.repeatCount = Float.infinity
 //
-//greenView.layer.addAnimation(cornerRadiusAnimation, forKey: "sqaure to circle")
-//greenView.layer.cornerRadius = greenView.frame.width/2
+//greenView.layer.addAnimation(cornerRadiusAnimation, forKey: "greenViewCornerRadius")
 
 
+//3. Keyframe Animation. Moving along path
 
-//Keyframe Animation. Moving along path
-
+//: ![Alternate text](3-square-along-path.gif width="width" height="height" poster="poster")
 
 //let greenView = UIView(frame: CGRect(x: 16, y: 239, width: 50, height: 50))
 //greenView.backgroundColor = UIColor.greenColor()
-////greenView.center = containerView.center
+//greenView.center = containerView.center
 //containerView.addSubview(greenView)
 //
 //
 //let path = UIBezierPath()
 //path.moveToPoint(CGPoint(x: 16,y: 239))
 //path.addCurveToPoint(CGPoint(x: 301, y: 239), controlPoint1: CGPoint(x: 136, y: 373), controlPoint2: CGPoint(x: 178, y: 110))
-//
 //
 //let pathAnimation = CAKeyframeAnimation(keyPath: "position")
 //pathAnimation.path = path.CGPath
@@ -53,72 +60,11 @@ containerView.addSubview(greenView)
 //
 //greenView.layer.addAnimation(pathAnimation, forKey: "move around in square")
 
-//Basic Animation. Make a red cicle change from a circle to a square.
 
+// 4. Group Animation
 
-//let circle = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-//circle.center = containerView.center
-//circle.layer.cornerRadius = 25
-//circle.backgroundColor = UIColor.redColor()
-//containerView.addSubview(circle)
-//
-//let cornerRadiusAnimation = CABasicAnimation(keyPath: "cornerRadius")
-//cornerRadiusAnimation.fromValue = 25
-//cornerRadiusAnimation.toValue = 0
-//cornerRadiusAnimation.duration = 2
-//
-//circle.layer.addAnimation(cornerRadiusAnimation, forKey: "move in box")
-//
-//circle.layer.cornerRadius = 0
+//: ![Alternate text](1-green-circle.png width="width" height="height" poster="poster")
 
-
-//Keyframe Animation. Moving along path
-
-//let circle = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-//circle.center = containerView.center
-//circle.layer.cornerRadius = 25
-//circle.backgroundColor = UIColor.redColor()
-//containerView.addSubview(circle)
-//
-//let path = UIBezierPath()
-//path.moveToPoint(containerView.center)
-//path.addLineToPoint(CGPoint(x: 0, y: containerView.center.y))
-//path.addLineToPoint(CGPoint(x: 0, y: 0))
-//path.addLineToPoint(CGPoint(x: containerView.center.x, y: 0))
-//path.closePath()
-//
-//let animation = CAKeyframeAnimation(keyPath: "position")
-//animation.path = path.CGPath
-//animation.duration = 2
-//
-//circle.layer.addAnimation(animation, forKey: "move along path")
-
-
-
-//Keyframe Animation. Moving along a funkier path
-
-//let circle = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-//circle.center = containerView.center
-//circle.layer.cornerRadius = 25
-//circle.backgroundColor = UIColor.redColor()
-//containerView.addSubview(circle)
-//
-//let path = UIBezierPath()
-//path.moveToPoint(CGPoint(x: 16,y: 239))
-//path.addCurveToPoint(CGPoint(x: 301, y: 239), controlPoint1: CGPoint(x: 136, y: 373), controlPoint2: CGPoint(x: 178, y: 110))
-//
-//let animation = CAKeyframeAnimation(keyPath: "position")
-//animation.path = path.CGPath
-//animation.duration = 2
-//animation.rotationMode = kCAAnimationRotateAuto
-//animation.repeatCount = Float.infinity
-//
-//
-//circle.layer.addAnimation(animation, forKey: "move along path")
-
-
-
-// Group Animation
 
 //let circle = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 50.0, height: 50.0))
 //circle.center = containerView.center
@@ -126,21 +72,11 @@ containerView.addSubview(greenView)
 //circle.backgroundColor = UIColor.redColor()
 //containerView.addSubview(circle);
 //
-//circle.layer.zPosition = 1
-//
-//let circle2 = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 50.0, height: 50.0))
-//circle2.center = containerView.center
-//circle2.layer.cornerRadius = 25.0
-//circle2.backgroundColor = UIColor.greenColor()
-//containerView.addSubview(circle2);
-//
-//circle2.layer.zPosition = -1
-//
-//let cornerRadius = CABasicAnimation()
-//cornerRadius.keyPath = "transform.scale"
-//cornerRadius.fromValue = 1
-//cornerRadius.toValue = 5
-//cornerRadius.duration = 2
+//let scaleUp = CABasicAnimation()
+//scaleUp.keyPath = "transform.scale"
+//scaleUp.fromValue = 1
+//scaleUp.toValue = 5
+//scaleUp.duration = 2
 //
 //let path = UIBezierPath()
 //path.moveToPoint(containerView.center)
@@ -153,12 +89,14 @@ containerView.addSubview(greenView)
 //position.duration = 2;
 //
 //let group = CAAnimationGroup()
-//group.animations = [position, cornerRadius]
+//group.animations = [position, scaleUp]
 //group.duration = 2
 //group.beginTime = 0
 //
-//circle2.layer.addAnimation(group, forKey: "moveCircle2")
+//circle.layer.addAnimation(group, forKey: "moveCircle2")
 //
-//circle2.layer.zPosition = 1
+//circle.layer.zPosition = 1
+
+
 
 
